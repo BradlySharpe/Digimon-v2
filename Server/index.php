3dd535a1e22df2197c9636ef9c3f7ae3fe1c355f
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-23 21:54:35 +1000

    - Set correct headers

--- Server/index.php
+++ Server/index.php
@@ -2,5 +2,9 @@
   require('Logic.php');
   require('Json.php');
   $json = new Json();
+  foreach ($json->getHeaders() as $value) {
+    header($value);
+  }
+
   $logic = new Logic($json);
   $logic->loginUser('fake@bradlysharpe.com.au', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8');
