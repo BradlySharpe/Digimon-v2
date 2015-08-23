a0e30d511acf065e013a09ec89ca9f707a9da63a
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-23 22:24:37 +1000

    - Added getState method

--- Server/index.php
+++ Server/index.php
@@ -1,6 +1,7 @@
 <?php
   require('Logic.php');
   require('Json.php');
+
   $json = new Json();
   foreach ($json->getHeaders() as $value) {
     header($value);
@@ -8,3 +9,5 @@
 
   $logic = new Logic($json);
   $logic->loginUser('fake@bradlysharpe.com.au', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8');
+  //Continue Game logic - dies if not logged in
+  $logic->getState();
