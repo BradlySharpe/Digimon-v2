a5baa156f47ce0ed9194e19d327b183cdeeb1ebf
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-08-23 22:24:11 +1000

    - Added fastForward method

--- Server/Logic.php
+++ Server/Logic.php
@@ -1,18 +1,38 @@
 <?php
   require('DBase.php');
+  require('User.php');
 
   class Logic {
     private $db;
     private $output;
+    private $user;
 
     public function __construct($_output) {
   		$this->output = $_output;
       $this->db = new DBase($this);
+      $this->user = new User($this->db);
   	}
 
-    public function loginUser($username, $password) {
-      $this->output->respond(array(
-        'loggedIn' => false
-      ));
+    public function loginUser($email, $password) {
+      if ($this->user->login($email, $password)) {
+        $this->fastForward();
+      } else
+        $this->output->error("Invalid Credentials");
     }
+
+    /**
+     * fastForward Run game logic while player was offline
+     */
+    private function fastForward() {
+
+    }
+
+    /**
+     * getState Get current game state
+     */
+    public function getState() {
+      $this->output->respond(['state' => []]);
+    }
+
+
   }
